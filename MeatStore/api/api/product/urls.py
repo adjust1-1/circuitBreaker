15da820d20e975322a16ea657daee4ab322b900b
adjust1-1 <adjust1@protonmail.com>
2024-03-12 23:07:32 +0200

    improve error handling

---
+++ MeatStore/api/api/product/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import ProductList, ProductDetail
+
+urlpatterns = [
+    path('products/', ProductList.as_view(), name='product-list'),
+    path('products/detail/<int:pk>/',
+         ProductDetail.as_view(), name='product-detail'),
+]
