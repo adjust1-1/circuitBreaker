ec4fb5851558488c672b1b52b07a281451e7ece4
adjust1-1 <adjust1@protonmail.com>
2024-04-25 23:07:32 +0300

    Improve performance of data processing

---
+++ MeatStore/api/api/cart/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from accounts.models import UserAccount
+from api.product.models import Product
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(UserAccount, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Корзина {self.user.email}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def sub_total(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"Товар козины: {self.product.title}"
